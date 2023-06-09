3f5fe95c66da62bad2e1fb5203343e1182731889
Alexander Paniagua <alexanderj146@gmail.com>
2023-06-09 18:10:37 -0300

    segunda PreEntrega

---
+++ PRE-ENTREGA 2/logica.js
@@ -0,0 +1,46 @@
+
+console.table(productos);
+let contenedorProds = document.getElementById('misprods');
+
+//Función para poder filtrar por precio
+function filtrarPorPrecio(precio){
+    const filtrados = productos.filter((prod)=>prod.precio <= precio);
+    return filtrados;
+}
+
+//dato de entrada
+let precioMax = parseFloat(prompt('Ingresa el precio máximo que puedes abonar 0-para salir'));
+
+while(precioMax != 0){
+    if(precioMax < 0){
+        alert('Ingrese un numero valido');
+    }else{
+        //ya tenemos un numero valido
+        const prodsFiltrados = filtrarPorPrecio(precioMax);
+        console.table(prodsFiltrados);
+        renderizarProductos(prodsFiltrados);
+        break
+    }
+    precioMax = parseFloat(prompt('Ingresa el precio máximo que puedes abonar 0-para salir'));
+}
+
+//DOM
+function renderizarProductos(listaProds){
+    //vaciamos en contenedor para evitar duplicados
+    contenedorProds.innerHTML='';
+    //cargamos las cartas de los productos solicitados
+    for(const prod of listaProds){
+        contenedorProds.innerHTML+=`
+            <div class="card col-sm-2">
+                <img class="card-img-top" src=${prod.foto} alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${prod.nombre}</h5>
+                    <p class="card-text">$ ${prod.precio}</p>
+                    <button class="btn btn-primary">Comprar</button>
+                </div>
+            </div>
+        `;
+    }
+}
+
+//renderizarProductos(productos);
---
+++ PRE-ENTREGA 2/productos.js
@@ -0,0 +1,37 @@
+const productos = [{
+        id: 1,
+        foto: href="./AURICULARES_IMG/apple.png",
+        nombre: "Airpods Pro",
+        precio: 95000
+    },
+    {
+        id: 2,
+        foto: href="./AURICULARES_IMG/Audio-Technica_1.png",
+        nombre: "ATH 5 ",
+        precio: 45000
+    },
+    {
+        id: 3,
+        foto: href="./AURICULARES_IMG/Beats_1.png",
+        nombre: "Beats by DRE",
+        precio: 26300
+    },
+    {
+        id: 4,
+        foto: href="./AURICULARES_IMG/Master_&_Dynamic_1.png",
+        nombre: "Master & Dynamic",
+        precio: 93000
+    },
+    {
+        id: 5,
+        foto: href="./AURICULARES_IMG/Samsung.png",
+        nombre: "Samsung Galaxy Buds 2",
+        precio: 50000
+    },
+    {
+        id: 6,
+        foto: href="./AURICULARES_IMG/SONY_1.png",
+        nombre: "Sony XM4",
+        precio: 130000
+    }
+];
